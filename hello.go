79883a134c35133f94b8dfca3eec0d4ed3cba686
Daniele Megna <daniele.megna@xpeppers.com>
2020-01-23 15:42:22 +0100

    Add and use morestrings package with ReverseRunes function

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "xp/med/hello/morestrings"
+)
 
 func main() {
-  fmt.Println("Hello, World!")
+  fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 }
