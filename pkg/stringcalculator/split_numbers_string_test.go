a6c228cde43e66ed580a36f5b4832945106c777e
Daniele Megna <daniele.megna@xpeppers.com>
2020-01-30 15:00:36 +0100

    Write some tests for SplitNumbersString

---
+++ pkg/stringcalculator/split_numbers_string_test.go
@@ -0,0 +1,19 @@
+package stringcalculator
+
+import (
+  "testing"
+  "github.com/stretchr/testify/assert"
+)
+
+func TestSingleIntegerString(t *testing.T) {
+  assert.Equal(t, []string{"19"}, SplitNumbersString("19"))
+}
+
+func TestSingleFloatWithDecimalsString(t *testing.T) {
+  assert.Equal(t, []string{"19.9"}, SplitNumbersString("19.9"))
+}
+
+func TestSomeNumbersSeparatedByComma(t *testing.T) {
+  expected := []string{"19", "46", "3.1415"}
+  assert.Equal(t, expected, SplitNumbersString("19,46,3.1415"))
+}
